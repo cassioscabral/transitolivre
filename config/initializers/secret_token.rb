d82ede34d0d694d2752fe21ad7b89bf43794bce9
Cassio Cabral <cassioscabral@gmail.com>
2013-10-01 20:46:59 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dadosabertos::Application.config.secret_key_base = 'a743e3ce72f59bd7bb01bc097f2fc8d48406676f2131b87301f26d6f7dfcd2e02a5b31071bfa31b010a82b3aefd3afc8603ef6000cc38a6eaaef3dbfd9a3d53c'
